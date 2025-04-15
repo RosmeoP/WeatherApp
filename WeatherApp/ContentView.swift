352aa3475a3719ecbfa72497472e8ea1e430ef5f
Mauricio <vrosmeo@gmail.com>
2025-04-15 14:00:05 -0600

    refatoring the code to make it cleaner

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -10,27 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("LightBlue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            BackgroundView(topColor: .blue, bottomColor:Color("LightBlue"))
             VStack{
-                Text("Cupertino, El Salvador")
-                    .font(.system(size:32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing: 8){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: (.fit))
-                        .frame(width: 180, height: 180)
-                    Text("76º")
-                        .font(.system(size:70, weight: .medium))
-                        .foregroundColor(.white)
-
-                }
-                .padding(.bottom )
+               CityInfoView(cityText: "Cupertino, Jucuapa")
+                primaryInfoView(imageName: "cloud.sun.fill", temperatureDay: 72)
+                
                 
                 HStack(spacing:18){
                     WeatherDayView(dayOfWeek: "TUE",                                        imageDay: "cloud.sun.fill",                                 temperatureDay: 72)
@@ -90,5 +74,58 @@ struct WeatherDayView: View {
             
             
         }
+    }
+}
+
+struct  BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityInfoView: View {
+    
+    var cityText: String
+    
+    var body: some View {
+        Text(cityText)
+            .font(.system(size:32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct primaryInfoView: View {
+    
+    
+    var imageName: String
+    var temperatureDay: Int
+    
+    
+    var body: some View {
+        
+        
+        
+
+        
+        VStack(spacing: 8){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: (.fit))
+                .frame(width: 180, height: 180)
+            Text("\(temperatureDay)º")
+                .font(.system(size:70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom )
     }
 }
